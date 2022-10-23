c0e00efc5618d1f271bde927ea479fcf5fca4d38
sebastien-salva <33250889+sebsalva@users.noreply.github.com>
2022-10-23 11:01:37 +0200

    Update MaxPvScript.lua
    synchronisation of states between MaxPv and Domoticz

--- MaxPvScript.lua
+++ MaxPvScript.lua
@@ -42,7 +42,7 @@
 --------------------------------
 -- variable à éditer ------
 local debugging = false --true pour voir les logs dans la console log Dz ou false pour ne pas les voir
-local adrRouteur = '192.168.1.17' --adr IP du routeur
+local adrRouteur = '192.168.1.17' --adr IP du routeur@
 local Energie = 'Routeur_Energie' -- Compteur Energie Consommée
 local idxCptEnergie = 12153 -- laisser à 0 si non utilisé
 local EnergieRoute = 'Routeur_EnergieRoute' -- Compteur Energie Routée
@@ -50,7 +50,7 @@ local idxCptEnergieRoute = 12152 -- laisser à 0 si non utilisé
 local EnergiePv = 'Routeur_Pv' -- Compteur Energie Produite, compteur d'impulsion
 local idxCptPv = 0 -- laisser à 0 si non utilisé
 local EnergieExp = 'Routeur_Export' -- Compteur Energie Exportée
-local idxCptExp = 0 -- laisser à 0 si non utilisé
+local idxCptExp = 12200 -- laisser à 0 si non utilisé
 local modeSSR = 'Routeur_SSR' -- Selecteur pour commander le SSR via MaxPV
 local modeRelais = 'Routeur_Relais' -- Selecteur pour commander le relais via MaxPV
 local EtatSSR ='Etat_SSR' -- Interupteur d'état du SSR 
@@ -58,6 +58,7 @@ local EtatRelais='Etat_Relais' -- Interupteur d'état du Relais
 -- fin variable à éditer
 --------------------------------
 local index=-1
+local delay=5
 --------------------------------
 --fonction d'affichage de Logs
 function see_logs (s)
@@ -88,36 +89,48 @@ function update(device, id, power, energy)
     commandArray[index] = {['UpdateDevice'] = id .. "|0|" .. power .. ";" .. energy}
     return
 end
+--fonction time difference
+function timediff (mode)
+    s = otherdevices_lastupdate[mode]
+    t1 = os.time()
+    year = string.sub(s, 1, 4)
+    month = string.sub(s, 6, 7)
+    day = string.sub(s, 9, 10)
+    hour = string.sub(s, 12, 13)
+    minutes = string.sub(s, 15, 16)
+    seconds = string.sub(s, 18, 19)
+    t2 = os.time{year=year, month=month, day=day, hour=hour, min=minutes, sec=seconds}
+    return os.difftime (t1, t2)
+end
 --fonction change etat Interupteur SSR Relais
-function ChangeState(statessr,staterelais)
+function ChangeState(statessr,staterelais,ch)
     -- mise à jour des etats des selecteurs dans domoticz (pour l'instant en attente)
-    --14 : mode de fonctionnement du SSR (0 = STOP, 1 = FORCE, 9 = AUTO)
-    --local modes = {
-    --    [0]=0;
-    --    [1]=10;
-    --    [9]=20;
-    --    ['stop']=0;
-    --    ['force']=1;
-    --    ['auto']=9;
-    --    };
---local sEtatSSR =otherdevices['Routeur_SSR'];
---local sEtatRelais =otherdevices['Routeur_Relais'];
---if statessr~=modes[sEtatSSR] then
---    commandArray[EtatSSR]='Set Level '..modes[statessr];
- --   see_logs('SSR changing state: '..statessr);
---end
---if staterelais~=modes[sEtatRelais] then
---    commandArray[EtatRelais]='Set Level '..modes[staterelais];
---    see_logs('Relais changing state old new: '..modes[sEtatRelais] .. staterelais);
---end
-if string.sub(tab[20],8,8) == '1' and otherdevices[EtatSSR]=='Off' then
+    local modes = {
+        ['0']=0;
+        ['1']=10;
+        ['9']=20;
+        ['stop']='0';
+        ['force']='1';
+        ['auto']='9';
+        };
+local sEtatSSR =otherdevices[modeSSR];
+local sEtatRelais =otherdevices[modeRelais];
+if statessr~=modes[sEtatSSR] and timediff(modeSSR) >=delay then
+    commandArray[modeSSR]='Set Level '..modes[statessr];
+    see_logs('SSR changing state: '..statessr);
+end
+if staterelais~=modes[sEtatRelais] and timediff(modeRelais) >=delay then
+    commandArray[modeRelais]='Set Level '..modes[staterelais];
+    see_logs('Relais changing state old new: '..modes[sEtatRelais] .. staterelais);
+end
+if string.sub(ch,8,8) == '1' and otherdevices[EtatSSR]=='Off' then
     commandArray[EtatSSR]='On' 
-elseif string.sub(tab[20],8,8) == '0' and otherdevices[EtatSSR]=='On' then
+elseif string.sub(ch,8,8) == '0' and otherdevices[EtatSSR]=='On' then
     commandArray[EtatSSR]='Off' 
 end
-if string.sub(tab[20],6,6) == '1' and otherdevices[EtatRelais]=='Off' then
+if string.sub(ch,6,6) == '1' and otherdevices[EtatRelais]=='Off' then
     commandArray[EtatRelais]='On' 
-elseif string.sub(tab[20],6,6) == '0' and otherdevices[EtatRelais]=='On' then
+elseif string.sub(ch,6,6) == '0' and otherdevices[EtatRelais]=='On' then
     commandArray[EtatRelais]='Off' 
 end
 end
@@ -135,12 +148,12 @@ end
 end
 commandArray = {}
 --selecteur selectionné ?
-if devicechanged[modeSSR ] or devicechanged[modeRelais] then 
+if devicechanged[modeSSR] or devicechanged[modeRelais] then 
     updaterouter(devicechanged[modeSSR],devicechanged[modeRelais])
 else
--- mise à jour toutes les 5 secondes pour éviter une surcharge
+-- mise à jour toutes les delay secondes pour éviter une surcharge
 time = os.date("*t")
-if time.sec %5 == 0 then 
+if time.sec % delay == 0 then 
 Cmd ='curl http://'..adrRouteur..'/api/get?alldata'
 config=assert(io.popen(Cmd))
             ret = config:read('*all')
@@ -150,7 +163,7 @@ if idxCptEnergie >0 then update(Energie, idxCptEnergie, tab[3], tab[10]) end
 if idxCptEnergieRoute >0 then update(EnergieRoute, idxCptEnergieRoute, tab[5], tab[9]) end
 if idxCptPv >0 then update(EnergiePv, idxCptPv, tab[13], tab[12]) end
 if idxCptExp >0 then update(EnergieExp, idxCptExp, tab[7], tab[11]) end
-ChangeState(tab[20])
+ChangeState(tab[14],tab[15],tab[20])
 end
 end
 return commandArray
